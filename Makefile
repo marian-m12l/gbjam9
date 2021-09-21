cb60f940aa332f4f796619d6225ad582f733cd48
marian <marian.muller@serli.com>
2021-09-21 13:18:35 +0200

    display background + character metasprite

--- Makefile
+++ Makefile
@@ -1,12 +1,32 @@
-CC = /opt/gbdk-linux/bin/lcc -Wa-l -Wl-m -Wl-j
+CC = /opt/gbdk-linux/bin/lcc
+PNG2MTSPR = /opt/gbdk-linux/bin/png2mtspr
 
-BINS = gbjam9.gb
+CFLAGS = -Wa-l -Wl-m -Wl-j
 
-all: $(BINS)
+TARGET = gbjam9.gb
 
-%.gb: %.c
-	$(CC) -o $@ $<
+ASSETS = $(wildcard assets/*.png)
+ASSETS_SRC = $(ASSETS:.png=.c)
+ASSETS_HEADERS = $(ASSETS:.png=.h)
+ASSETS_OBJ = $(ASSETS:.png=.o)
+
+SRC = $(wildcard *.c)
+OBJ = $(SRC:.c=.o)
+
+all: $(TARGET)
+
+%.c: %.png
+	$(PNG2MTSPR) $< -c $@ -sh 16 -spr8x8 -sp 0x10
+
+%.o: %.c
+	$(CC) $(CFLAGS) -o $@ -c $<
+
+$(TARGET): $(ASSETS_SRC) $(OBJ)
+	$(CC) $(CFLAGS) -o $@ $^
+
+run: $(TARGET)
+	higan $(TARGET)
 
 clean:
-	rm -f *.o *.lst *.map *.gb *~ *.rel *.cdb *.ihx *.lnk *.sym *.asm *.noi
+	rm -rf *.o *.lst *.map *.gb *~ *.rel *.cdb *.ihx *.lnk *.sym *.asm *.noi $(ASSETS_SRC) $(ASSETS_HEADERS) $(ASSETS_OBJ)
 
--- gbjam9.c
+++ gbjam9.c
@@ -1,14 +1,94 @@
+#include <gb/console.h>
 #include <gb/gb.h>
-#include <stdint.h>
-#include <stdio.h>
+#include <gb/metasprites.h>
 
-#include <gb/bgb_emu.h>
+#include "assets/bird.h"
+
+const unsigned char pattern[] = {0x80,0x80,0x40,0x40,0x20,0x20,0x10,0x10,0x08,0x08,0x04,0x04,0x02,0x02,0x01,0x01};
+
+joypads_t joypads, prevJoypads;
+
+// Character metasprite tiles are loaded into VRAM starting at tile number 0
+#define BIRD_TILE_NUM_START 0
+// Character metasprite will be built starting with hardware sprite 0
+#define BIRD_SPR_NUM_START 0
+
+// Character position
+uint16_t posX, posY;
+uint8_t idx, rot;
 
 
 void main() {
-    printf("Hello, world!");
+    // Init palettes
+    BGP_REG = 0xE4;     // BG Palette : Black, Dark gray, Light gray, White
+    OBP0_REG = 0xE4;    // Sprite palette 0 : Black, Dark gray, Light gray, Transparent
+    OBP1_REG = 0xE0;    // Sprite palette 1 : Black, Dark gray, White, Transparent
+
+	// Fill the screen background with a single tile pattern
+    fill_bkg_rect(0, 0, 20, 18, 0);
+    set_bkg_data(0, 1, pattern);
+
+    // Load character metasprite tile data into VRAM
+    set_sprite_data(BIRD_TILE_NUM_START, sizeof(bird_data) >> 4, bird_data);
+
+    // Show background and sprites
+    SHOW_BKG; SHOW_SPRITES;
     
-    while (1) {
+    // Use 8x8 sprites/tiles
+    SPRITES_8x8;
+
+    // Init joypad
+    joypad_init(1, &joypads);
+ 
+    // Set initial position
+    posX = posY = 64 << 4;
+    idx = 2; rot = 0;
+
+    while(1) {        
+        // Poll joypad
+        prevJoypads = joypads;
+        joypad_ex(&joypads);
+        
+        // Handle movements
+        if (joypads.joy0 & J_UP) {
+            posY -= 16;
+        } else if (joypads.joy0 & J_DOWN) {
+            posY += 16;
+        }
+
+        if (joypads.joy0 & J_LEFT) {
+            posX -= 16;
+            rot = 0;
+        } else if (joypads.joy0 & J_RIGHT) {
+            posX += 16;
+            rot = 1;
+        }
+
+
+        // Press A button to show/hide metasprite
+        if ((joypads.joy0 & J_A) && !(prevJoypads.joy0 & J_A)) {
+            posX = posY = 64 << 4;
+        }
+
+        // TODO Handle all animation cycles
+        // Press B button to cycle through metasprite animations FIXME Autmatic animation !!!
+        if (joypads.joy0 & J_B) {
+            idx++;
+            if (idx > 9) idx=2;
+        }
+
+        uint8_t hiwater = 0;
+        // FIXME Should only be called when something changed
+        switch (rot) {
+            case 0: hiwater = move_metasprite       (bird_metasprites[idx], BIRD_TILE_NUM_START, BIRD_SPR_NUM_START, (posX >> 4), (posY >> 4)); break;
+            case 1: hiwater = move_metasprite_vflip (bird_metasprites[idx], BIRD_TILE_NUM_START, BIRD_SPR_NUM_START, (posX >> 4), (posY >> 4)); break;
+        };
+
+        // Hide rest of the hardware sprites, because amount of sprites differs between animation frames.
+        for (uint8_t i = hiwater; i < 40; i++) shadow_OAM[i].y = 0;
+
+        // Wait for VBlank
         wait_vbl_done();
     }
 }
+
